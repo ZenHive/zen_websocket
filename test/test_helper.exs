da443bedaf3b24a0535a5d7ab0c9f7c46b8dbf02
E.FU <e-fu@users.noreply.github.com>
2025-11-05 12:18:23 -0400

    refactor: clean up test_helper.exs for improved readability
    Remove verbose testing policy warnings and commented-out code to streamline test configuration.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,36 +1,6 @@
 require Logger
 ## Configure Logger to only show warnings and errors
-# Logger.configure(level: :none)
+# [:none, :info, :warning, :debug]
 Logger.configure(level: :info)
-# Logger.configure(level: :warning)
-# Logger.configure(level: :debug)
 
 ExUnit.start(exclude: [:stability, :stability_dev])
-# Display testing policy message
-Logger.warning("""
-\n[!] TESTING POLICY [!]
---------------------------------------------------
-DO NOT USE MOCKS! They are proven to be unreliable.
-ALWAYS test against real test APIs (e.g., test.deribit.com).
-If no test API is available, use real API.
-
-NEVER SKIP TESTS because you can not find CREDENTIALS. Credentials
-must be always present. LET THEM FAIL.
-
-Failure to adhere to this policy will result in immediate
-TERMINATION ("you are fired!") and your employment will be terminated.
---------------------------------------------------
-\n
-""")
-
-# ZenWebsocket uses a simpler architecture without complex behaviors
-# Only mock what's actually needed for testing
-
-# Mox.set_mox_global()
-
-### see in mix.exs where it is loaded already
-# ["#{__DIR__}", "support/**/*.ex"]
-# |> Path.join()
-# |> Path.wildcard()
-# |> Enum.each(&Code.require_file/1)
-### end of see in mix.exs where it is loaded already
